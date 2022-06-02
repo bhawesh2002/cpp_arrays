6a6255ec830c2bfeb966c57e66b25d91bb69b93b
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-02 21:19:31 +0530

    use colon instead of semi-colon

--- k largest elements/kLargest.cpp
+++ k largest elements/kLargest.cpp
@@ -7,7 +7,7 @@ int main()
     int num[5];
     for (int i = 0; i < sizeof(num) / sizeof(int); i++)
     {
-        cout << "Enter the " << i + 1 << " number; ";
+        cout << "Enter the " << i + 1 << " number: ";
         cin >> num[i];
     }
     for (int i = 0; i < sizeof(num)/sizeof(int); i++)
