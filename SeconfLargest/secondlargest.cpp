a375c00fbf9145f33f8a30839c1ef67e36d663f9
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-02 20:15:18 +0530

    for loop to fill the arrayy

--- SeconfLargest/secondlargest.cpp
+++ SeconfLargest/secondlargest.cpp
@@ -5,5 +5,10 @@ using namespace std;
 int main()
 {
     int num[5];
+    for (int i = 0; i < sizeof(num) / sizeof(int); i++)
+    {
+        cout << "Enter the " << i + 1 << " number:";
+        cin >> num[i];
+    }
     return 0;
 }
