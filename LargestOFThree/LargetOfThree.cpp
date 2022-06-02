cebc939b685e7e82c9b6f886d47012b551fa36e1
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-02 08:14:22 +0530

    fill array eith array elements as numbers

--- LargestOFThree/LargetOfThree.cpp
+++ LargestOFThree/LargetOfThree.cpp
@@ -4,5 +4,10 @@ using namespace std;
 
 int main()
 {
-    
+    int num[7] = {};
+    for (int i = 0; i < sizeof(num) / sizeof(int); i++)
+    {
+        cout << "Enter the " << i + 1 << " number:";
+        cin >> num[i];
+    }
 }
