1a98b3a6f09790eed9a0e3c1cd37fddb06f1eaef
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-02 22:53:59 +0530

    create a foor loop

--- greater elements/greaterelements.cpp
+++ greater elements/greaterelements.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main()
 {
-    int num [5] = {95,645,884,331,994};
+    int num[5] = {95, 645, 884, 331, 994};
+    for (int i = 0; i < sizeof(num) / sizeof(num[0]); i++)
+    {
+        /* code */
+    }
+
     return 0;
 }
