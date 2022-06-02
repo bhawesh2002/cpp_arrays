ec29ea63a616d604e1d48b8fb92f93f02d564073
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-02 07:57:09 +0530

    code to compare array elements

--- Largest and smallest/LandS.cpp
+++ Largest and smallest/LandS.cpp
@@ -5,9 +5,22 @@ using namespace std;
 int main()
 {
     int num[5] = {};
+    int largest;
     for (int i = 0; i < sizeof(num) / sizeof(int); i++)
     {
         cout << "Enter the " << i + 1 << " number: ";
         cin >> num[i];
     }
+    for (int i = 0; i < sizeof(num) / sizeof(int) - 1; i++)
+    {
+        if (num[i] > num[i + 1])
+        {
+            largest = num[i];
+        }
+        else
+        {
+            largest = num[i + 1];
+        }
+    }
+    return 0;
 }
