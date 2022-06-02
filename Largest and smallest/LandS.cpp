10b2de74b097ebc0f760ca30a16650ae397eb734
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-02 07:54:21 +0530

    use of for loop to fill the array

---
+++ Largest and smallest/LandS.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num[5] = {};
+    for (int i = 0; i < sizeof(num) / sizeof(int); i++)
+    {
+        cout << "Enter the " << i + 1 << " number: ";
+        cin >> num[i];
+    }
+}
