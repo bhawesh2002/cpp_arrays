2117583a6127de97f7a02b8e5f4ad6af20faa9e9
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-02 23:16:32 +0530

    remove everything

--- greater elements/greaterelements.cpp
+++ greater elements/greaterelements.cpp
@@ -7,14 +7,6 @@ int main()
     int num[7] = {2, 7, 3, 5, 4, 6, 8};
     for (int i = 0; i < sizeof(num) / sizeof(num[0]); i++)
     {
-        for (int j = 0; j < sizeof(num) / sizeof(num[0]); j++)
-        {
-            if (num[j] > num[i])
-            {
-                cout << num[i] << "=>" << num[j] << endl;
-                break;
-            }
-        }
     }
 
     return 0;
