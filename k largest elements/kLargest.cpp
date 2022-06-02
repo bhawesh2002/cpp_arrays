9d506b313636d475acaeee38514b78d141e31ccc
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-02 21:15:50 +0530

    SOME CHANGES

--- k largest elements/kLargest.cpp
+++ k largest elements/kLargest.cpp
@@ -7,9 +7,8 @@ int main()
     int num[5];
     for (int i = 0; i < sizeof(num) / sizeof(int); i++)
     {
-        cout << "Enter the " << i + 1 << " number";
+        cout << "Enter the " << i + 1 << " number; ";
         cin >> num[i];
     }
-
     return 0;
 }
