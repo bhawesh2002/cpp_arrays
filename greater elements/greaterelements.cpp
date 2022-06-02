affdc7f9472fa61590101951dc7db34c141d73c6
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-02 22:59:32 +0530

    code

--- greater elements/greaterelements.cpp
+++ greater elements/greaterelements.cpp
@@ -7,11 +7,11 @@ int main()
     int num[5] = {95, 645, 884, 331, 994};
     for (int i = 0; i < sizeof(num) / sizeof(num[0]); i++)
     {
-        for (int j = 0; i < sizeof(num) / sizeof(num[0]) - 1; i++)
+        for (int j = 0; j < sizeof(num) / sizeof(num[0]); j++)
         {
-            if (num[j] < num[i])
+            if (num[j] > num[i])
             {
-                cout << num[i] << "=>" << num[j];
+                cout << num[i] << "=>" << num[j] << endl;
                 break;
             }
         }
