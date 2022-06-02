b6663f443063d4ebc66d809865374c96f13857bb
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-02 08:41:11 +0530

    user filled array

--- LargestOFThree/LargetOfThree.cpp
+++ LargestOFThree/LargetOfThree.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int main()
 {
-    int num[7] = {54, 89, 4566, 655, 54, 394, 4};
-    /* for (int i = 0; i < sizeof(num) / sizeof(int); i++)
+    int num[7] = {};
+    for (int i = 0; i < sizeof(num) / sizeof(int); i++)
      {
          cout << "Enter the " << i + 1 << " number:";
          cin >> num[i];
-     }*/
+     }
     for (int i = 0; i < sizeof(num) / sizeof(int); i++)
     {
         for (int j = 0; j < sizeof(num) / sizeof(int) - 1; j++)
