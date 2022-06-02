cea82b98589746da0c54c78a659fdaceac72354e
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-02 23:28:51 +0530

    major code changes

--- greater elements/greaterelements.cpp
+++ greater elements/greaterelements.cpp
@@ -5,9 +5,25 @@ using namespace std;
 int main()
 {
     int num[7] = {2, 7, 3, 5, 4, 6, 8};
-    for (int i = 0; i < sizeof(num) / sizeof(num[0]); i++)
+    int counter = 0;
+    int j = 0;
+    for (int i = 0; i < sizeof(num) / sizeof(num[0]) - 1; i++)
     {
-    }
+        while (1)
+        {
+            if (num[j] > num[i])
+            {
+                cout << num[i] << "=>" << num[j] << endl;
+                counter++;
+                j = counter;
 
+                break;
+            }
+            else
+            {
+                j++;
+            }
+        }
+    }
     return 0;
 }
