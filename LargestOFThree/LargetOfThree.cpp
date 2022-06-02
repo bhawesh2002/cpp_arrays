c5612035884ac193d52c103388c61f27b2f7f52a
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-02 08:20:59 +0530

    attempt to sort array

--- LargestOFThree/LargetOfThree.cpp
+++ LargestOFThree/LargetOfThree.cpp
@@ -11,4 +11,14 @@ int main()
         cin >> num[i];
     }
     int largest[3] = {};
+    int index = 0;
+    for (int i = 0; i < sizeof(num) / sizeof(int) - 1; i++)
+    {
+        for (int j = 1; j < sizeof(num) / sizeof(int); j++)
+        {
+            if (num[j] > num[j])
+            {a
+            }
+        }
+    }
 }
