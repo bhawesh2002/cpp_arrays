d60bc2786fcf78e02923d3695974fe61de677040
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-03 20:08:08 +0530

    code to check repeating numbers

--- repeating numbers/RepeatingNumbers.cpp
+++ repeating numbers/RepeatingNumbers.cpp
@@ -7,5 +7,16 @@ int main()
     int num[5] = {5, 5, 5, 64, 64};
     int counter = 0;
     int a = sizeof(num) / sizeof(num[0]);
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; i < a; i++)
+        {
+            if (num[i] == num[j])
+            {
+                counter++;
+            }
+        }
+    }
+
     return 0;
 }
