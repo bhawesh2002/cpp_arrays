3b7457409675c58cd3472bab360e86bd0f228856
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-03 19:06:39 +0530

    int variable storing the no of elements in array

--- second smllest/SecondSmallest.cpp
+++ second smllest/SecondSmallest.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 int main()
 {
-    int num[5] = {32,24,56,233,87};
-    for (int i = 0; i < sizeof(num)/sizeof(num[0]); i++)
+    int num[5] = {342, 24, 556, 23, 857};
+    int a = sizeof(num) / sizeof(num[0]);
+    for (int i = 0; i < a; i++)
     {
-        
+        /* code */
     }
-    
+
     return 0;
 }
