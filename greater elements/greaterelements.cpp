595d105dde0d7fe2f1e1ae8a0b0fbdee418d607f
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-03 07:57:01 +0530

    get the desired o/p

--- greater elements/greaterelements.cpp
+++ greater elements/greaterelements.cpp
@@ -9,7 +9,7 @@ int main()
     int a = sizeof(num) / sizeof(num[0]);
     for (int i = 0; i < a; i++)
     {
-        for (int j = i; j < a; j++)
+        for (int j = i+1; j < a; j++)
         {
             if (num[j] > num[i])
             {
