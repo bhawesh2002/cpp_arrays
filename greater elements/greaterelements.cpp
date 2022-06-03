27c7af2913df6bcd2c0c7f3569f101a3253131a9
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-03 07:36:34 +0530

    start from scratch

--- greater elements/greaterelements.cpp
+++ greater elements/greaterelements.cpp
@@ -5,29 +5,5 @@ using namespace std;
 int main()
 {
     int num[7] = {7,6,5,4,3,2,1};
-    int counter = 0;
-    int j = 0;
-    for (int i = 0; i < sizeof(num) / sizeof(num[0]); i++)
-    {
-        while (1)
-        {
-            if (num[j] > num[i])
-            {
-                cout << num[i] << "=>" << num[j] << endl;
-                counter++;
-                j = counter;
-                break;
-            }
-            else
-            {
-                j++;
-            }
-        }
-    }
-    /*for (int i = 0; i < sizeof(num) / sizeof(num[0]); i++)
-    {
-        cout <<  i  << endl;
-    }*/
-
     return 0;
 }
