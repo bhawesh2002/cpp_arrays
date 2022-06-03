c4b73b7e8aa6ab0bbc5c1875d47e27c0e44f4493
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-03 08:15:29 +0530

    change the elements

--- greater elements/greaterelements.cpp
+++ greater elements/greaterelements.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main()
 {
-    int num[7] = {7,6,5,4,3,2,1};
-    int largest = 0;
+    int num[7] = {1,2,3,4,5,6,7};
+    int largest = -1;
     int a = sizeof(num) / sizeof(num[0]);
     for (int i = 0; i < a; i++)
     {
