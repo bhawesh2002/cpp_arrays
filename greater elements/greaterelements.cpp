ed56785ea4bcfdbc7fdc56d402cd1eff4d9a8412
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-03 08:09:49 +0530

    declare a largest variable to store greate element

--- greater elements/greaterelements.cpp
+++ greater elements/greaterelements.cpp
@@ -4,24 +4,20 @@ using namespace std;
 
 int main()
 {
-    int num[7] = {17, 18, 19, 20, 21, 22, 23};
-    int index = 0;
+    int num[7] = {17, 118, 1, 24, 31, 27, 321};
+    int largest = 0;
     int a = sizeof(num) / sizeof(num[0]);
     for (int i = 0; i < a; i++)
     {
-        for (int j = i+1; j < a; j++)
+        for (int j = i + 1; j < a; j++)
         {
             if (num[j] > num[i])
             {
-                cout << num[i] << "=>" << num[j] << endl;
-                break;
-            }
-            else
-            {
-                cout << num[i] << "=>" << -1 << endl;
+                largest = num[j];
                 break;
             }
         }
+        cout << num[i] << "=>" << largest << endl;
     }
 
     return 0;
