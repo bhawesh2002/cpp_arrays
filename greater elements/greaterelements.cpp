04f222007c47812256622ac2db7a2aaf482a8075
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-03 07:40:14 +0530

    use of for loop

--- greater elements/greaterelements.cpp
+++ greater elements/greaterelements.cpp
@@ -7,6 +7,9 @@ int main()
     int num[7] = {7, 6, 5, 4, 3, 2, 1};
     int index = 0;
     int a = sizeof(num) / sizeof(num[0]);
-
+    for (int i = 0; i < a; i++)
+    {
+    }
+    
     return 0;
 }
