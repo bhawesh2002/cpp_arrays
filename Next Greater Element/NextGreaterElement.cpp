e2a24b1af5e466d0179120f7bd6b236ae56f601c
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-04 09:07:40 +0530

    code changes

--- Next Greater Element/NextGreaterElement.cpp
+++ Next Greater Element/NextGreaterElement.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main()
 {
-    int num[5] = {7, 6, 5, 4, 3};
+    int num[5] = {3, 4, 5, 6, 7};
     int a = sizeof(num) / sizeof(num[0]);
     for (int i = 0; i < a; i++)
     {
         for (int j = i; j < i + 1; j++)
         {
-            if (num[i] < num[j])
+            if (num[i] < num[j + 1])
             {
                 cout << num[i] << "=>" << num[j] << endl;
             }
