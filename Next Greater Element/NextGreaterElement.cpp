00efaa8e6ceb3f56f990e968aee879f41bdf74c7
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-04 09:14:24 +0530

    major changes

--- Next Greater Element/NextGreaterElement.cpp
+++ Next Greater Element/NextGreaterElement.cpp
@@ -4,19 +4,15 @@ using namespace std;
 
 int main()
 {
-    int num[5] = {7,6,5,4,3};
+    int num[5] = {3,4,5,6,7};
     int a = sizeof(num) / sizeof(num[0]);
     for (int i = 0; i < a; i++)
     {
-        for (int j = i; j < i + 1; j++)
+        for (int j = i; j < i + 2; j++)
         {
-            if (num[i] < num[j + 1])
+            if (num[i] < num[j])
             {
                 cout << num[i] << "=>" << num[j] << endl;
-            }
-            else
-            {
-                cout << num[i] << " has no greater element adjcent to it" << endl;
             }
         }
     }
