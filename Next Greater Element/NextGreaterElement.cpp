d532a4c19d34f3e51f8f6c3d7a80d006bb34b32e
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-04 09:00:45 +0530

    code to determine adjcent greater element

--- Next Greater Element/NextGreaterElement.cpp
+++ Next Greater Element/NextGreaterElement.cpp
@@ -6,5 +6,17 @@ int main()
 {
     int num[5] = {5, 6545, 1, 54, 45};
     int a = sizeof(num) / sizeof(num[0]);
+    for (int i = 0; i < a - 1; i++)
+    {
+        if (num[i] < num[i + 1])
+        {
+            cout << num[i] << "=>" << num[i + 1] << endl;
+        }
+        else
+        {
+            cout << num[i] << " has no greater element adjcent to it" << endl;
+        }
+    }
+
     return 0;
 }
