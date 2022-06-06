7d0fe19ebe0c0cbef02ef7f002a0f7ba7f12b599
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-06 07:16:28 +0530

    basic structure of the program

--- Wave Sort/WaveSort.cpp
+++ Wave Sort/WaveSort.cpp
@@ -1,2 +1,11 @@
 /*Write a C++ program to sort a given unsorted array of integers, in wave form.
 Note: An array is in wave form when array[0] >= array[1] <= array[2] >= array[3] <= array[4] >= . . . .*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
