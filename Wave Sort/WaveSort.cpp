47ef9cad664f945d615687b600463998ae41c6f0
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-06 07:15:41 +0530

    format of wave form in this question

---
+++ Wave Sort/WaveSort.cpp
@@ -0,0 +1,2 @@
+/*Write a C++ program to sort a given unsorted array of integers, in wave form.
+Note: An array is in wave form when array[0] >= array[1] <= array[2] >= array[3] <= array[4] >= . . . .*/
