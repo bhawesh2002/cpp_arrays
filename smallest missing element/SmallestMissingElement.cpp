90c70dbe9d1507d93941367e5003577ed3bf383f
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-09 08:09:09 +0530

    code to find the missing element

--- smallest missing element/SmallestMissingElement.cpp
+++ smallest missing element/SmallestMissingElement.cpp
@@ -6,6 +6,21 @@ int main()
 {
     int sorted[] = {0, 1, 2, 3, 5, 6, 7, 8, 9};
     int a = sizeof(sorted) / sizeof(int);
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = i + 1; i < a - 1; i++)
+        {
+            if (sorted[j] == (sorted[i] + 1))
+            {
+                break;
+            }
+            else
+            {
+                cout << "Missing number is " << sorted[i] + 1 << endl;
+                break;
+            }
+        }
+    }
 
     return 0;
 }
