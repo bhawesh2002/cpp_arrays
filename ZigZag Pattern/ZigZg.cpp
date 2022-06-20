f9cc631d4570380e12faed93a474fd7f1c81cbc5
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-20 12:59:40 +0530

    basic structure of the program

--- ZigZag Pattern/ZigZg.cpp
+++ ZigZag Pattern/ZigZg.cpp
@@ -1,3 +1,10 @@
 /*12. Write a C++ program to rearrange the elements of a given array of integers in zig-zag fashion way.
 Note: The format zig-zag array in form a < b > c < d > e < f.*/
 
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    return 0;
+}
