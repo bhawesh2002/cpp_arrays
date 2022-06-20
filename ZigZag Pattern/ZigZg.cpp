60a0d45541418f30a717f07e76fe11df1147c07b
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-20 13:11:35 +0530

    code

--- ZigZag Pattern/ZigZg.cpp
+++ ZigZag Pattern/ZigZg.cpp
@@ -2,12 +2,28 @@
 Note: The format zig-zag array in form a < b > c < d > e < f.*/
 
 #include <iostream>
-
+#include <cstdlib>
 using namespace std;
 
 int main()
 {
     num[] = {23,43,53,65,23,54}
     int size = sizeof(num)/sizeof(num[0]);
+    for(int i = 0; i < size; i++)
+    {
+        if(i % 2 == 0)
+        {
+            if(num[i + 1] < num[i])
+            {
+                swap(num[i],num[i+ 1]);
+            }
+        }else if(i % 2 != 0)
+        {
+            if(num[i+ 1]>num[i])
+            {
+                swap(num[i],num[i+1]);
+            }
+        }
+    }
     return 0;
 }
