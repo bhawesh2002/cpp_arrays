b61352a662d8e6de88f1cd8307b404d61ad3b2c6
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-20 12:50:15 +0530

    answer to the question

--- multiply by next and previous.cpp
+++ multiply by next and previous.cpp
@@ -8,5 +8,19 @@ using namespace std;
 int main(){
     int num[] = {2,34,56,76,76,90}
     int prev = 1,next = 1;
+    int size = sizeof(num)/sizeof(int);
+    for(int i = 0; i < size;i++)
+    {
+        if(i == 0)
+        {
+            num[i] = 1 * num[i + 1];
+        }else if(i == (size - 1))
+        {
+            num[i] = num[i-1] * 1; 
+        }else
+        {
+            num[i] = num[i-1] * num[i+ 1];
+        }
+    }
     return 0;
 }
