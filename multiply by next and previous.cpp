363acdfa5c8180fec3ca0ca013a9b3abd662dd3c
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2022-06-20 12:40:44 +0530

    basic structure of the program

--- multiply by next and previous.cpp
+++ multiply by next and previous.cpp
@@ -1,2 +1,10 @@
 /* Write a C++ program to update every array element by multiplication of 
 next and previous values of a given array of integers*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    return 0;
+}
